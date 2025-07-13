19dc3d488e31f3ca5693849309a77e419ac8bf43
Mahamadou Kante <mahakantem@gmail.com>
2025-07-13 14:25:33 +0000

    build

---
+++ --typescript/dist/pages/index.astro.mjs
@@ -0,0 +1,26 @@
+import { c as createComponent, d as createAstro, a as addAttribute, e as renderHead, r as renderTemplate } from '../chunks/astro/server_CAgZhzEv.mjs';
+import 'kleur/colors';
+import 'html-escaper';
+import 'clsx';
+export { renderers } from '../renderers.mjs';
+
+const $$Astro = createAstro();
+const $$Index = createComponent(($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Index;
+  return renderTemplate`<html lang="en"> <head><meta charset="utf-8"><link rel="icon" type="image/svg+xml" href="/favicon.svg"><meta name="viewport" content="width=device-width"><meta name="generator"${addAttribute(Astro2.generator, "content")}><title>Astro</title>${renderHead()}</head> <body> <h1>Astro</h1> </body></html>`;
+}, "C:/Users/ISH/Documents/GitHub/failtrack/--typescript/src/pages/index.astro", void 0);
+
+const $$file = "C:/Users/ISH/Documents/GitHub/failtrack/--typescript/src/pages/index.astro";
+const $$url = "";
+
+const _page = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+	__proto__: null,
+	default: $$Index,
+	file: $$file,
+	url: $$url
+}, Symbol.toStringTag, { value: 'Module' }));
+
+const page = () => _page;
+
+export { page };
